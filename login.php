d1155420ef6d7eb687ba74c19baa78b4a825b72f
iixann <ivangonzalolap@gmail.com>
2025-06-11 19:22:04 -0300

    commitlogin

---
+++ login.php
@@ -0,0 +1,29 @@
+<?php
+session_start();
+if (isset($_SESSION['usuario'])) {
+    header("Location: bienvenida.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Login</title>
+</head>
+<body>
+    <h2>Iniciar sesión</h2>
+    <form method="POST" action="procesar_login.php">
+        <label>Usuario:</label>
+        <input type="text" name="usuario" required><br><br>
+        <label>Contraseña:</label>
+        <input type="password" name="password" required><br><br>
+        <input type="submit" value="Ingresar">
+    </form>
+    <?php
+    if (isset($_GET['error'])) {
+        echo "<p style='color:red'>Usuario o contraseña incorrectos.</p>";
+    }
+    ?>
+</body>
+</html>
